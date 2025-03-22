4b8718f65cafbd48506605b56dd4d73bfe7b0e8f
Ahmed Drioueche <adsrahmed@gmail.com>
2025-03-22 20:03:01 +0100

    fixed errorr

--- src/types/general.ts
+++ src/types/general.ts
@@ -1,7 +1,3 @@
-import { dict } from "../utils/dict";
-
-export type Language = keyof typeof dict;
-
 export type Theme = "light" | "dark" | "system";
 
 export type ActionResult = "error" | "success" | "loading" | "idle";
