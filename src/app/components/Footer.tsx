ec3eeb41a30aec8549f6ac18fdd742d32d43c380
Ahmed Drioueche <adsrahmed@gmail.com>
2025-05-07 15:01:44 +0100

    fix

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Linkedin, Github, Twitter, Mail, Code, Facebook } from "lucide-react";
+import { Linkedin, Github, Code, Facebook } from "lucide-react";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
